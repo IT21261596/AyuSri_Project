691a2421155f11739b0e5ebda5dc95a6392afc3a
msi <it21265006@my.sliit.lk>
2023-05-05 16:51:02 +0530

    customer login view

---
+++ app/src/main/java/com/example/ayusri/cusloginmain.kt
@@ -0,0 +1,11 @@
+package com.example.ayusri
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class cusloginmain : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cusloginmain)
+    }
+}
