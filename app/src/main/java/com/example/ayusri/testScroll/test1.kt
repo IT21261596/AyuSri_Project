11e2d73875ca83097452959c9afd8f4ae6542b34
devsandanayake <devinsandanayake49@gmail.com>
2023-05-06 16:32:03 +0530

    doctor cunsultation

---
+++ app/src/main/java/com/example/ayusri/testScroll/test1.kt
@@ -0,0 +1,12 @@
+package com.example.ayusri.testScroll
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ayusri.R
+
+class test1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test1)
+    }
+}
